d48f7762930686f52f596f0c665e556f70adfe26
ericy51 <ericy330@gmail.com>
2016-11-13 21:12:50 -0800

    most recent update

--- exercise_2/ex2_setup.sh
+++ exercise_2/ex2_setup.sh
@@ -6,23 +6,22 @@ mkdir /home/w205/exercise_2
 cp -a ./ /home/w205/exercise_2
 
 #wordcount folder
-su - w205
-sparse quickstart wordcount
-rm wordcount/src/spouts/words.py
-rm wordcount/src/bolts/wordcount.py
-rm wordcount/topologies/wordcount.clj
+
+su - w205 -c "sparse quickstart wordcount"
+su - w205 -c "rm wordcount/src/spouts/words.py"
+su - w205 -c "rm wordcount/src/bolts/wordcount.py"
+su - w205 -c "rm wordcount/topologies/wordcount.clj"
 
 #move files to streaming directories
-cp exercise_2/tweets.py wordcount/src/spouts
-cp exercise_2/parse.py wordcount/src/bolts
-cp exercise_2/wordcount.py wordcount/src/bolts
-cp exercise_2/tweetwordcount.clj wordcount/topologies
+su - w205 -c "cp exercise_2/tweets.py wordcount/src/spouts"
+su - w205 -c "cp exercise_2/parse.py wordcount/src/bolts"
+su - w205 -c "cp exercise_2/wordcount.py wordcount/src/bolts"
+su - w205 -c "cp exercise_2/tweetwordcount.clj wordcount/topologies"
 
 #Postgres Setup
-initdb -D /home/w205/data
-pg_ctl -D /home/w205/data -l logfile start
+su - w205 -c "initdb -D /home/w205/data"
+su - w205 -c "pg_ctl -D /home/w205/data -l logfile start"
 
-python /home/w205/exercise_2/postgres_create_db.py
+su - w205 -c "python /home/w205/exercise_2/create_postgres_db.py"
 
-cd wordcount
-sparse run
+su - w205 -c "cd wordcount && sparse run"
