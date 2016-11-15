181226e30e4c1c2326e59a9e0ac0ee032c00ba2a
ericy51 <ericy330@gmail.com>
2016-11-15 10:12:56 -0800

    updated

--- exercise_2/ex2_setup.sh
+++ exercise_2/ex2_setup.sh
@@ -1,3 +1,7 @@
+#Postgres Setup
+su - w205 -c "initdb -D /home/w205/data"
+su - w205 -c "pg_ctl -D /home/w205/data -l logfile start"
+
 pip install psycopg2
 pip install tweepy
 
@@ -6,7 +10,6 @@ mkdir /home/w205/exercise_2
 cp -a ./ /home/w205/exercise_2
 
 #wordcount folder
-
 su - w205 -c "sparse quickstart wordcount"
 su - w205 -c "rm wordcount/src/spouts/words.py"
 su - w205 -c "rm wordcount/src/bolts/wordcount.py"
@@ -18,10 +21,8 @@ su - w205 -c "cp exercise_2/parse.py wordcount/src/bolts"
 su - w205 -c "cp exercise_2/wordcount.py wordcount/src/bolts"
 su - w205 -c "cp exercise_2/tweetwordcount.clj wordcount/topologies"
 
-#Postgres Setup
-su - w205 -c "initdb -D /home/w205/data"
-su - w205 -c "pg_ctl -D /home/w205/data -l logfile start"
-
+#Create database and table
 su - w205 -c "python /home/w205/exercise_2/create_postgres_db.py"
 
+#Run Streamparse
 su - w205 -c "cd wordcount && sparse run"
